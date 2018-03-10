8e51338da06015c19585e869d90380a01ad5f155
Phil Henault <philhen27@gmail.com>
2018-03-09 20:19:35 -0500

    able to register multiple users and store in DB, need to do more validation

--- app.py
+++ app.py
@@ -1,39 +1,23 @@
-#online task keeper
-
 from flask import Flask , render_template, flash, redirect, url_for, session, logging, request
-from flask_mysqldb import MySQL
 from data import Tasks
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 import os
-
-
-
+import pymysql.cursors
 
 app = Flask(__name__)
 
 tasks = Tasks()
 
-#config
-app.config['mysql_host'] = 'localhost'
-app.config['mysql_user'] = os.environ.get('DB_USER')
-app.config['mysql_password'] = os.environ.get('DB_PASS')
-app.config['mysql_db'] = 'notekeeper'
-app.config['mysql_cursorclass'] = 'DictCursor'
-
-#initialize 
-mysql = MySQL(app)
-
-
 
 
 @app.route('/')
 def index():
-		return render_template('home.html')
+        return render_template('home.html')
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
 
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
@@ -54,24 +38,29 @@ def register():
         username = form.username.data
         password = sha256_crypt.encrypt(str(form.password.data))
 
-        # Create cursor
-        cur = mysql.connection.cursor()
+        un = os.environ['DB_USER']
+        pw = os.environ['DB_PASS']
+        connection = pymysql.connect(host='localhost', 
+                                     user=un,
+                                     password=pw,
+                                     db='notekeeper',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
 
-        # Execute query
+
+        cur = connection.cursor() 
         cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-
-        # Commit to DB
-        mysql.connection.commit()
-
-        # Close connection
-        cur.close()
-
+        connection.commit()
         flash('You are now registered and can log in', 'success')
+        cur.close()
+        connection.close()
+        
+        return redirect(url_for('index'))
 
-        return redirect(url_for('login'))
+        
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
-	app.secret_key = 'secret12345'
-	app.run(debug = True)
-	
+    app.secret_key = 'secret12345'
+    app.run(debug = True)
+    
